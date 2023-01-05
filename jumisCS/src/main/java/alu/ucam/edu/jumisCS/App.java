f1e6a6628cf041b76777a4c6fa36b97e61085eef
Manuel <manuelvillegasobregon@gmail.com>
2023-01-05 17:47:07 +0100

    Primer commit

---
+++ jumisCS/src/main/java/alu/ucam/edu/jumisCS/App.java
@@ -0,0 +1,13 @@
+package alu.ucam.edu.jumisCS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jumisCS/src/test/java/alu/ucam/edu/jumisCS/AppTest.java
@@ -0,0 +1,20 @@
+package alu.ucam.edu.jumisCS;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
